f98bed572c42c7acb03424c2fb8b061ed32054ee
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2026-01-29 04:57:16 +0300

    Add logging to BeerController for debugging purposes

--- src/main/java/dev/dead/spring7reactivewebflux/controller/BeerController.java
+++ src/main/java/dev/dead/spring7reactivewebflux/controller/BeerController.java
@@ -3,6 +3,7 @@ package dev.dead.spring7reactivewebflux.controller;
 import dev.dead.spring7reactivewebflux.model.BeerDTO;
 import dev.dead.spring7reactivewebflux.services.BeerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,11 +13,12 @@ import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class BeerController {
     public static final String BEER_PATH = "/api/v2/beer";
-    public static final String BEER__PATH_ID = "/{beerId}";
+    public static final String BEER__PATH_ID = BEER_PATH + "/{beerId}";
     private final BeerService beerService;
 
     @DeleteMapping(BEER__PATH_ID)
@@ -38,11 +40,13 @@ public class BeerController {
 
     @GetMapping(BEER_PATH)
     Flux<BeerDTO> getAllBeers() {
+        log.debug("Get all beers");
         return beerService.getAllBeers();
     }
 
     @GetMapping(BEER__PATH_ID)
-    Mono<BeerDTO> getBeerById(Integer beerId) {
+    Mono<BeerDTO> getBeerById(@PathVariable Integer beerId) {
+        log.debug("Get beer by id: {}", beerId);
         return beerService.getBeerById(beerId)
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)));
     }
